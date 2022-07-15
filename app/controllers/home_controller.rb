2d716fce4b70cb6fc8fe7bea7498f5392754db20
Shivani Shilpi <shivani.shilpi@clecotech.com>
2022-07-15 14:54:50 +0530

    some changes

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,7 @@
 class HomeController < ApplicationController
   def index
     @restaurants = Restaurant.all  
+
     if current_user.has_role? :owner 
       if current_user.stripe_id.nil? 
         redirect_to subscriptions_path
